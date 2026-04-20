6ab954af7b43d07ad4b586c30d67e1be318e068e
Thomas Cormican <thomascormican123@gmail.com>
2026-04-20 21:12:21 +0100

    update

--- assignment 4/server.js
+++ assignment 4/server.js
@@ -34,12 +34,19 @@ app.get("/books", (req, res) => {
   const status = req.query.status; //3.3
   if (!status) {
     db.all("SELECT * FROM books", (err, rows) => {
-      res.json(rows);
+      if (err) {
+        res.status(500).json({ message: err });
+      } else {
+        res.json(rows);
+      }
     });
   } else {
     db.all("SELECT * FROM books WHERE status = ?", [status], (err, rows) => {
-      if (err) console.log(err);
-      res.json(rows);
+      if (err) {
+        res.status(500).json({ message: err });
+      } else {
+        res.json(rows);
+      }
     });
   }
 });
@@ -48,7 +55,9 @@ app.get("/books", (req, res) => {
 app.get("/books/:id", (req, res) => {
   const id = req.params.id;
   db.all("SELECT * FROM books WHERE id = ?", [id], (err, rows) => {
-    if (err) return console.error(err);
+    if (err) {
+      return res.status(500).json({ message: err });
+    }
 
     if (rows.length === 0) {
       res.status(404).json({ message: "Book not found" });
@@ -62,16 +71,21 @@ app.get("/books/:id", (req, res) => {
 app.post("/books", (req, res) => {
   const { title, author, year, status } = req.body;
   if (!title || !author || !status) {
-    res.json({ message: "title, author and status must be provided" });
+    res
+      .status(400)
+      .json({ message: "title, author and status must be provided" });
   } else if (!validateStatus(status)) {
-    res.json({ message: "invalid status" });
+    res.status(400).json({ message: "invalid status" });
   } else {
     db.run(
       `INSERT INTO books (title, author, year, status) VALUES (?, ?, ?, ?)`,
       [title, author, year, status],
       function (err) {
-        if (err) console.log(err);
-        res.json({ id: this.lastID });
+        if (err) {
+          res.status(500).json({ message: err });
+        } else {
+          res.json({ id: this.lastID });
+        }
       },
     );
   }
@@ -80,21 +94,24 @@ app.post("/books", (req, res) => {
 //4.2
 app.put("/books/:id", (req, res) => {
   const id = req.params.id;
-  db.all("SELECT * FROM books WHERE id = ?", [id], (err, rows) => {
+  db.all("SELECT * FROM books WHERE id = ?", [id], (error, rows) => {
+    if (error) {
+      return res.status(500).json({ message: error });
+    }
     if (rows.length === 0) {
       return res.status(404).json({ message: "Book not found" });
     } else {
       const { title, year, status } = req.body;
 
       if (status && !validateStatus(status)) {
-        return res.json({ message: "invalid status" });
+        return res.status(400).json({ message: "invalid status" });
       }
 
       const updateSql =
         "UPDATE books SET title = COALESCE(?, title), year = COALESCE(?, year), status = COALESCE(?, status) WHERE id = ?";
       db.run(updateSql, [title, year, status, id], (err) => {
         if (err) {
-          res.json({ message: "err" });
+          res.status(500).json({ message: "err" });
         } else {
           res.json({ message: `updated book ${id}` });
         }
@@ -108,12 +125,15 @@ app.delete("/books/:id", (req, res) => {
   const id = req.params.id;
 
   // Check if it exists
-  db.all("SELECT * FROM books WHERE id = ?", [id], (err, rows) => {
+  db.all("SELECT * FROM books WHERE id = ?", [id], (error, rows) => {
+    if (error) {
+      return res.status(500).json({ message: error });
+    }
     if (rows.length === 0) {
       res.status(404).json({ message: "Book not found" });
     } else {
       db.run("DELETE FROM books WHERE id = ?", [id], (err) => {
-        if (err) res.json({ message: "error deleting" });
+        if (err) res.status(500).json({ message: "error deleting" });
         else res.json({ message: "Book deleted successfully" });
       });
     }
